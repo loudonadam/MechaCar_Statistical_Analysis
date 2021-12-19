f9ba61f8d806cafc6c694c6330f3f106216bf15b
Adam Loudon <loudon.adam7@gmail.com>
2021-12-19 17:06:53 -0500

    final r file save

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,3 +16,8 @@ total_summary <- coil_data %>% summarize(Mean = mean(PSI), Median = median(PSI),
 
 lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>%summarize(Mean = mean(PSI), Median = median(PSI), Variance=var(PSI),SD=sd(PSI))
 
+t.test(coil_data$PSI,mu=1500)
+
+t.test(subset(coil_data, Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(coil_data, Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(coil_data, Manufacturing_Lot=="Lot3")$PSI,mu=1500)
